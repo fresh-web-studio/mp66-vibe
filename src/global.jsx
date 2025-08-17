55b2ea6431defd900ba0401823c7a9b1008985d3
Andrey Kolchanov <eandy@inbox.ru>
2025-08-17 23:43:03 +0500

    Ошибка с путями...

--- src/global.jsx
+++ src/global.jsx
@@ -3,6 +3,10 @@ import { Head } from "minista"
 import Header from '@/layouts/Header'
 import Content from '@/layouts/Content'
 import Footer from '@/layouts/Footer'
+import appleTouchIcon from '@/assets/favicons/apple-touch-icon.png'
+import favicon32 from '@/assets/favicons/favicon-32x32.png'
+import favicon16 from '@/assets/favicons/favicon-16x16.png'
+import manifest from '@/assets/favicons/site.webmanifest'
 
 export default function (props) {
     const {
@@ -17,10 +21,10 @@ export default function (props) {
             <Head htmlAttributes={{ lang: 'ru' }}>
                 <title>{title}МеталлПром: прием металлолома, лом черных и цветных металлов в Нижнем Тагиле и Свердловской области</title>
                 <script src="/src/main.js" type='module' />
-                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
-                <link rel="manifest" href="/site.webmanifest" />
+                <link rel="apple-touch-icon" sizes="180x180" href={appleTouchIcon} />
+                <link rel="icon" type="image/png" sizes="32x32" href={favicon32} />
+                <link rel="icon" type="image/png" sizes="16x16" href={favicon16} />
+                <link rel="manifest" href={manifest} />
             </Head>
             <Header url={url} />
             <Content>
